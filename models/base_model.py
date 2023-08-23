01c3e848566be1f620f5b20bfb1a47be740cab23
Viestar <m.silverbuckson@gmail.com>
2023-08-23 18:49:26 +0300

    Task 6

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 from datetime import datetime
 from uuid import uuid4
+import models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
-class BaseModel:
-    """BaseModel defines all common attributes/methods for other classes"""
+class BaseModel():
+    """BaseModel defines all common attributes/methods for other classes
+    Attributes:
+        id (sqlalchemy String): The BaseModel id.
+        created_at (sqlalchemy DateTime): time instance is born.
+        updated_at (sqlalchemy DateTime): time instance is altered.
+    """
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Common attributes of all subclasses"""
-        from models import storage
+        """Initialising Common attributes of all subclasses"""
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -20,20 +32,15 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.age = None  # Add the age attribute
-            self.password = None  # Add the password attribute
-            self.email = None  # Add the email attribute
-            storage.new(self)
-
-    def __str__(self) -> str:
-        """Returns: [class name] (ID) <class dictionary>"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+            # self.age = None  # Add the age attribute
+            # self.password = None  # Add the password attribute
+            # self.email = None  # Add the email attribute
 
     def save(self):
         """Updates the public updated_at with the current datetime"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of the instance"""
@@ -43,4 +50,15 @@ class BaseModel:
             if key in ("created_at", "updated_at"):
                 value = self.__dict__[key].isoformat()
             object_dict[key] = value
+        for key, value in object_dict.items():
+            if key == "_sa_instance_state":
+                object_dict.pop(key)
         return object_dict
+
+    def delete(self):
+        """ Deletes current instance from the storage"""
+        models.storage.delete()
+
+    def __str__(self) -> str:
+        """Returns: [class name] (ID) <class dictionary>"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
--- models/city.py
+++ models/city.py
@@ -2,15 +2,21 @@
 """ Model for the city inheriting from the base model """
 
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """
     The City model
 
     Arguments:
+        __table__(str): Database table name
         state_id (str): Unique State id.
         name (str): State name.
     """
-    state_id = ""
-    name = ""
+    __table__ = "cities"
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
