ec5f22afa219bd5721bce46dbaeb39b55915f800
Viestar <m.silverbuckson@gmail.com>
2023-08-23 20:37:20 +0300

    Task 8

--- models/review.py
+++ models/review.py
@@ -2,6 +2,8 @@
 """ Model for the review inheriting from the base model """
 
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_switch
 
 
 class Review(BaseModel):
@@ -9,10 +11,17 @@ class Review(BaseModel):
     The Review model
 
     Arguments:
+        __tablename__: Database table
         place_id (str): Unique Place id.
         user_id (str): Unique user id.
         text (str): Review.
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    if storage_switch == 'db':
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
